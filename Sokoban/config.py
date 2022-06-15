43dc0497388ebfb95381b2bde65f4664ca8ebbbd
northernmelody <43698648+northernmelody@users.noreply.github.com>
2022-06-15 17:05:57 +0800

    Add files via upload

---
+++ Sokoban/config.py
@@ -0,0 +1,11 @@
+Config = {
+			'WIDTH': 500,
+			'HEIGHT': 500,
+			'block_size': 50,
+			'levels_path': 'Sokoban/levels',
+			'resources_path': 'Sokoban/resources',
+			'imgfolder': 'imgs',
+			'fontfolder': 'font',
+			'audiofolder': 'audios',
+			'bg_color': (45, 45, 45)
+		}
